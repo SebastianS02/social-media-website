a35d32d32818ef1759998f2a5dd14a9260ae7a12
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-01-19 17:13:19 -0500

    changed app and index files to .js

--- solo-website/src/pages/_app.js
+++ solo-website/src/pages/_app.js
@@ -1,8 +1,7 @@
 import '@/styles/globals.css'
 import Layout from '/components/layout.js'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }){
   return (
     <Layout>
       <Component {...pageProps} />
