5a252d7f596dc21a98587abba80f9c68241caf48
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-01-23 18:11:44 -0500

    changed document to .js, added new styling

--- solo-website/components/layout.js
+++ solo-website/components/layout.js
@@ -2,9 +2,10 @@ import Nav from './Nav';
 
 export default function Layout({children}){
     return (
-        <div>
+        //
+         <div className="mx-6 md:max-w-2x1 md:mx-auto font-sofiaSans"> 
             <Nav />
             <main>{children}</main>
-        </div>
+        </div> 
     )
 }
