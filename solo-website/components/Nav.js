e8fb3fa1de425c487f53ca09ef503d624647b49d
Sebastian Stec <sebastianstec@sebastians-mbp.myfiosgateway.com>
2023-04-16 16:08:09 -0400

    added home and join now buttons to nav bar

--- solo-website/components/Nav.js
+++ solo-website/components/Nav.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Nav(){
     return (
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <nav className="flex justify-between items-center py-10">
+            <Link href="/">
+                <button className="text-lg font-medium">Creative Minds</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link href={"/auth/login"} legacyBehavior>
+                    <a className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium ml-8">Join Now</a>
+                </Link>
+            </ul>
+        </nav>
     );
 }
