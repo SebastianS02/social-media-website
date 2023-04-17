8f3137d9592126e70f7f9cba9eeebd0873f0c676
Sebastian Stec <sebastianstec@Sebastians-MBP.lan>
2023-04-17 15:06:33 -0400

    added logic for join now button if user is already logged in

--- README.md
+++
@@ -1,2 +0,0 @@
-# social-media-website
-Following an online tutorial for full stack web development to create a website where a user can create an account, log in, and post on a dashboard. Other users can do the same, as well as comment on other users' posts.
--- solo-website/src/pages/auth/login.js
+++ solo-website/src/pages/auth/login.js
@@ -1,19 +1,32 @@
 import { FcGoogle } from "react-icons/fc";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../../utils/firebase";
+import { useRouter } from 'next/router';
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useEffect } from "react";
 
 export default function Login() {
+    const route = useRouter();
+    const [user, loading] = useAuthState(auth);
 
     //Sign in with Google
     const googleProvider = new GoogleAuthProvider();
     const GoogleLogin = async () => {
         try {
             const result = await signInWithPopup(auth, googleProvider);
-            console.log("Signed in")
+            route.push("/");
         } catch (error) {
             console.log(error)
         }
-    }
+    };
+
+    useEffect(() => {
+        if(user) {
+            route.push("/");
+        } else {
+            console.log("login");
+        }
+    }, [user]);
 
     return(
         <div className="shadow-x1 mt-32 p-10 text-gray-7 rounded-lg">
