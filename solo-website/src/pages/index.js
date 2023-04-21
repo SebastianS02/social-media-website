84d913cfe972914436999585bbaa85c3384030ea
Sebastian Stec <sebastianstec@Sebastians-MacBook-Pro.local>
2023-04-21 14:58:19 -0400

    Posts now appear on dashboard

--- solo-website/src/pages/index.js
+++ solo-website/src/pages/index.js
@@ -1,6 +1,26 @@
 import Head from 'next/head'
+import Message from "../../components/message"
+import { useEffect, useState } from "react"
+import { db } from "../../utils/firebase"
+import { collection, orderBy, query, onSnapshot } from "firebase/firestore"
 
 export default function Home() {
+  //Create a state with all posts
+  const [allPosts, setAllPosts] = useState([]);
+
+  const getPosts = async () => {
+    const collectionRef = collection(db, 'posts');
+    const q = query(collectionRef, orderBy('timestamp', 'desc'));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setAllPosts(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+    })
+    return unsubscribe;
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
   return (
     <div>
       <Head>
@@ -9,9 +29,14 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <div className="my-12 text-lg font-medium">
         <h2 className="text-2x1">See what other people are saying!</h2>
+        {allPosts.map((post) => (
+          <Message {...post}>
+            
+          </Message>
+        ))}
       </div>
     </div>
   );
