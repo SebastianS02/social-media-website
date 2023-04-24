98078d5d565fd247e3821a9366108b063cf3b3d3
Sebastian Stec <sebastianstec@Sebastians-MacBook-Pro.local>
2023-04-24 14:35:21 -0400

    readded react icons

--- solo-website/src/pages/dashboard.js
+++ solo-website/src/pages/dashboard.js
@@ -4,6 +4,8 @@ import { useRouter } from "next/router"
 import { useState, useEffect } from "react"
 import { collection, query, doc, where, onSnapshot, deleteDoc } from 'firebase/firestore';
 import Message from "../../components/message"
+import { BsTrash2Fill } from "react-icons/bs"
+import { AiFillEdit } from "react-icons/ai"
 import Link from "next/link"
 
 export default function Dashboard() {
@@ -44,12 +46,12 @@ export default function Dashboard() {
                     return (
                         <Message {...post} key={post.id}>
                             <div className="flex gap-4">
-                                <button onClick={() => deletePost(post.id)} className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm">
-                                    Delete
+                                <button className="text-pink-600 flex items-center justify-center gap-2 py-2 text-sm">
+                                    <BsTrash2Fill onClick={() => deletePost(post.id)} className="text-2xl" />Delete
                                 </button>
                                 <Link href={{pathname: "/post", query: post}}>
                                     <button className="text-teal-600 flex items-center justify-center gap-2 py-2 text-sm">
-                                        Edit
+                                        <AiFillEdit className="text-2xl"/>Edit
                                     </button>
                                 </Link>
                             </div>
